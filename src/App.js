f443fbf936c39bc7dde7143b32408a9ce6064d3c
TrungNguyen <nguyenhuutrung@192.168.2.14>
2023-04-05 10:36:03 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Header from "./components/Header.js";
 import Homepage from "./pages/Homepage";
